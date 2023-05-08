c1050f0f0832e74ade27e7f635add56e06ff64fc
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-05-08 15:19:10 +0530

    added middlewares

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
 
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieparser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 app.all("*", require("./routes/root"), (req, res) => {
@@ -17,5 +30,7 @@ app.all("*", require("./routes/root"), (req, res) => {
     res.type("txt").send("404 not found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
