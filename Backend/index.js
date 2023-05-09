ab2025fc740d8b10dd64ecceaceb4fbd8e0cd2b0
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-05-09 18:27:37 +0530

    Added routes for users & notes

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const corsOptions = require("./config/corsOptions");
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
-
+const mongoDbConnect = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+mongoDbConnect();
 
 app.use(logger);
 
@@ -19,6 +25,8 @@ app.use(cookieparser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/userRoutes"));
 
 app.all("*", require("./routes/root"), (req, res) => {
   res.status(404);
@@ -33,4 +41,15 @@ app.all("*", require("./routes/root"), (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
